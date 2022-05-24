46cf6af86017d8176a1b4ee67a45be31c0bd4cbe
pan <panw3i@gmail.com>
2022-05-24 10:10:27 +0800

    About page

--- README.md
+++ README.md
@@ -8,4 +8,14 @@ yarn rw setup ui tailwindcss
 ```
 yarn rw prisma migrate dev
 yarn rw g scaffold post
+
+```
+
+
+```
+ 列表页
+ <Route path="/posts" page={PostPostsPage} name="posts" />
+    =>
+    pages/Post/PostsPage
+
 ```
---
+++ web/src/pages/Post/PostsPage/PostsPage.tsx
@@ -0,0 +1,12 @@
+import PostsCell from 'src/components/Post/PostsCell'
+
+const PostsPage = () => {
+  return (
+    <>
+      <PostsCell />
+      <h1 className={'text-orange-200'}>1</h1>
+    </>
+  )
+}
+
+export default PostsPage
